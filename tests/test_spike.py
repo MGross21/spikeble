5c61a9e52512486e8b2f58d54787897d08f33859
MGross21 <mgstinger521@gmail.com>
2025-08-10 02:01:32 -0700

    refactor: simplify test script by removing unnecessary async function and imports

--- tests/test_spike.py
+++ tests/test_spike.py
@@ -3,19 +3,11 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
-
-
-# example.py
 import asyncio
 from pathlib import Path
-from spike.spike import Spike, run
+from spike.spike import run
 
-EXAMPLE_SLOT = 0
 PROGRAM = Path(os.path.join(os.path.dirname(__file__), "__test.py")).read_bytes()
 
-async def main():
-    await run(PROGRAM)
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(run(PROGRAM))
